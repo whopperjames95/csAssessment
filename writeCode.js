e65f8000222a93d5dcc2f2c1f081b94672a52ea9
James Goff <james.goff1030@gmail.com>
2022-05-27 09:56:57 -0700

    initial commit

---
+++ writeCode.js
@@ -0,0 +1,61 @@
+//code1
+let array = [1,2,3,4,5,-5]
+
+let value = false;
+for (let i = 0; i < array.length; i++) {
+  for (let j = 0; j < array.length; j++) {
+    if (i !== j) {
+      if (array[i] + array[j] === 0) {
+        value = true;
+      } 
+    }
+  }
+}
+
+console.log(value)
+
+// space complexity: O(n)
+
+
+//code2
+let word = ('James Goff')
+
+function hasUniqueChars(word) {
+    let uniqueChars = new Set([])
+    for (let i = 0; i < word.length; i++) {
+      uniqueChars.add(word[i])
+    }
+    return uniqueChars.size === word.length
+}
+
+console.log(hasUniqueChars(word))
+// space complexity: O(n)
+
+
+//code3
+const isPangram = (string) => 
+     new Set(string.toLowerCase().match(/[a-z]/g)).size === 26;
+
+console.log(isPangram("abcdefghijklmnopqrstuvwxyz"));
+// space complexity: O(1)
+
+
+
+//code4
+function findLongestWord(str) {
+    var strSplit = str.split(' ');
+    var longestWord = 0;
+    for(var i = 0; i < strSplit.length; i++){
+      if(strSplit[i].length > longestWord){
+      longestWord = strSplit[i].length;
+       }
+    }
+    return longestWord;
+  }
+
+  console.log(findLongestWord("The quick brown fox jumpedweerfgbxcghfhdgggdhfg over the lazy dog"))
+  // space complexity: O(n)
+
+
+
+
